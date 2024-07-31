5e83002005356d5586c3b6e711f5d7030736d1db
v <v@mail.ru>
2024-07-31 23:14:32 +0500

    lesson_24: погодное приложение лог JSON

--- lesson_24.py
+++ lesson_24.py
@@ -2,6 +2,7 @@
 Lesson_24: JSON и YAML
 Picle - ?
 """
+
 # JSON - JavaScript Object Notation
 """
 Массив - список
@@ -18,11 +19,11 @@ FILE = "./data/lesson_24.json"
 
 # Чтение из JSON
 
-with open(FILE, "r", encoding="utf-8") as file:
-    data = json.load(file)
+# with open(FILE, "r", encoding="utf-8") as file:
+#     data = json.load(file)
 
-print(data)
-print(type(data))
+# print(data)
+# print(type(data))
 
 # Методы JSON
 # load - загрузка из файла
@@ -32,8 +33,8 @@ print(type(data))
 
 # JSON строка - список покупок
 
-json_shop_list = '["Шоколадный дед мороз", "Мандарины", "Шампанское", "Огурцы", "Кефир"]'
-print(type(json_shop_list))
+# json_shop_list = '["Шоколадный дед мороз", "Мандарины", "Шампанское", "Огурцы", "Кефир"]'
+# print(type(json_shop_list))
 
 # Задача - преобразовать это в JSON строку и обратно в список Python
 """
@@ -44,54 +45,54 @@ print(type(json_shop_list))
 - Это преобразует список в строку
 """
 
-shop_list = json.loads(json_shop_list)
-print(shop_list)
-print(type(shop_list))
-shop_list.append("фейрверк")
+# shop_list = json.loads(json_shop_list)
+# print(shop_list)
+# print(type(shop_list))
+# shop_list.append("фейрверк")
 
-json_shop_list = json.dumps(shop_list, ensure_ascii=False)
-print(json_shop_list)
+# json_shop_list = json.dumps(shop_list, ensure_ascii=False)
+# print(json_shop_list)
 
-print(json.loads(json_shop_list))
+# print(json.loads(json_shop_list))
 
 # Список словрей студентов. Фамилия, имя, возраст, группа, хобби
 
-students = [
-    {
-        "Фамилия": "Мишустин",
-        "Имя": "Иван",
-        "Возраст": 18,
-        "Группа": "ПИ-19-1",
-        "Хобби": ["налоги", "приливы", "отливы"],
-    },
-    {
-        "Фамилия": "Басков",
-        "Имя": "Петр",
-        "Возраст": 19,
-        "Группа": "ПИ-19-1",
-        "Хобби": ["пение", "шубы"],
-    },
-    {
-        "Фамилия": "Киркоров",
-        "Имя": "Сидор",
-        "Возраст": 20,
-        "Группа": "ПИ-19-1",
-        "Хобби": ["не те двери", "короны"],
-    },
-]
+# students = [
+#     {
+#         "Фамилия": "Мишустин",
+#         "Имя": "Иван",
+#         "Возраст": 18,
+#         "Группа": "ПИ-19-1",
+#         "Хобби": ["налоги", "приливы", "отливы"],
+#     },
+#     {
+#         "Фамилия": "Басков",
+#         "Имя": "Петр",
+#         "Возраст": 19,
+#         "Группа": "ПИ-19-1",
+#         "Хобби": ["пение", "шубы"],
+#     },
+#     {
+#         "Фамилия": "Киркоров",
+#         "Имя": "Сидор",
+#         "Возраст": 20,
+#         "Группа": "ПИ-19-1",
+#         "Хобби": ["не те двери", "короны"],
+#     },
+# ]
 
 
 # Запись в JSON файл
-with open(FILE, "w", encoding="utf-8") as file:
-    json.dump(students, file, ensure_ascii=False, indent=4)
+# with open(FILE, "w", encoding="utf-8") as file:
+#     json.dump(students, file, ensure_ascii=False, indent=4)
 
 
-# Чтение из JSON файла
-with open(FILE, "r", encoding="utf-8") as file:
-    data = json.load(file)
+# # Чтение из JSON файла
+# with open(FILE, "r", encoding="utf-8") as file:
+#     data = json.load(file)
 
 
-pprint(data)
+# pprint(data)
 
 # Команда обнволения pip - pip install --upgrade pip
 
@@ -141,15 +142,43 @@ weather_data = response.json()
 ["main"]["humidity"] - влажность
 ["wind"]["speed"] - скорость ветра
 """
+# Дата время - импорт
+from datetime import datetime
 
 final_weather_data = {
+    "дата": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
     "город": weather_data["name"],
     "погода": weather_data["weather"][0]["description"],
     "температура": weather_data["main"]["temp"],
     "ощущается как": weather_data["main"]["feels_like"],
     "влажность": weather_data["main"]["humidity"],
     "скорость ветра": weather_data["wind"]["speed"],
-
 }
 
-pprint(final_weather_data, sort_dicts=False)
+
+# Запись данных в CSV файл, windows-1251, lineterminator="\n", dictwriter
+import csv
+
+# CSV_FILE = "./data/lesson_24.csv"
+
+# with open(CSV_FILE, "a", encoding="windows-1251") as file:
+#     writer = csv.DictWriter(file, fieldnames=final_weather_data.keys(), lineterminator="\n",)
+#     writer.writeheader()
+#     writer.writerow(final_weather_data)
+
+# all_weather = []
+# all_weather.append(final_weather_data)
+
+# print(json.dumps(all_weather, ensure_ascii=False, indent=4))
+
+# 1. Читаю JSON - список словарей и ложу в переменную all_weather
+
+with open(FILE, "r", encoding="utf-8") as file:
+    all_weather = json.load(file)
+
+# 2. Добавляю в список словарей новую погоду
+all_weather.append(final_weather_data)
+
+# 3. Записываю в файл
+with open(FILE, "w", encoding="utf-8") as file:
+    json.dump(all_weather, file, ensure_ascii=False, indent=4)
