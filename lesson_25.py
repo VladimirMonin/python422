e5cce85a3aaf1ee79dab61f3afd6b0a7b3325011
v <v@mail.ru>
2024-08-19 23:33:54 +0500

    lesson_25: *args - множественые аргументы

--- lesson_25.py
+++ lesson_25.py
@@ -101,5 +101,92 @@ def main():
 name_25 = __name__
 print(name_25)
 
-if name_25 == "__main__":
-    main()
+num_list = [1, 2, 3, 4, 5]
+
+def get_sum_by_list(num_list):
+    result = 0
+    for num in num_list:
+        if not isinstance(num, int):
+            raise TypeError("Список должен содержать только числа")
+        result += num
+
+    return result
+
+# Функция с множественными параметрами *args
+print(1)
+print(1, 2)
+# print(1, 2, n...)
+
+def get_many_sum(*nums):
+    print(nums)# tuple (1, 2, 3, 4, 5)
+    print(type(nums)) # <class 'tuple'>
+    print(*nums) # 1 2 3 4 5
+    print(nums[0], nums[1], nums[2], nums[3], nums[4])
+    
+
+get_many_sum(1, 2, 3, 4, 5)
+get_many_sum('П', 'р', 'и', 'в', 'е', 'т')
+num_list = [1, 2, 3, 4, 5]
+get_many_sum(*num_list)
+get_many_sum(num_list[0], num_list[1], num_list[2], num_list[3], num_list[4])
+print(*num_list)
+
+a, b, c, d, e = num_list
+a, b, c, *other = num_list
+
+print(other)
+
+"""
+1.
+Напишите функцию которая принимает *args слов
+и возвращает их количество
+2.
+Напишите функцию которая принимает *args слов
+проверяет их на палиндромы и возвращает список палиндромов
+word.lower() == word.lower()[::-1]
+3.
+Напишите функцию которая принимает *args слов
+проверяет их на палиндромы и возвращает словарь формата {слово: True/False}
+"""
+
+# 1. Напишите функцию которая принимает *args слов
+def count_words(*args):
+    return len(args)
+
+words = ['дед', 'казак', 'шалаш', 'репа']
+
+print(count_words(*words))
+print(count_words(words[0], words[1], words[2], words[3]))
+print(count_words('дед', 'казак', 'шалаш', 'репа'))
+
+#2. Напишите функцию которая принимает *args слов и проверяет их на палиндромы и возвращает список палиндромов
+
+def get_list_palindromes(*words):
+    palindromes = []
+    for word in words:
+        if word.lower() == word.lower()[::-1]:
+            palindromes.append(word)
+    return palindromes
+
+print(get_list_palindromes(*words))
+
+# 3. Функция которая принимает *args слов и проверяет их на палиндромы и возвращает словарь формата {слово: True/False}
+
+def get_dict_palindromes(*words):
+    palindromes = {}
+    words = {word.lower() for word in words}
+    
+    # 3. Dict comprehension
+    result = {word: word.lower() == word.lower()[::-1] for word in words}
+
+    # 1. Работа со словарями в цикле
+    for word in words:
+        palindromes[word] = word.lower() == word.lower()[::-1]
+        # 2. Работа со словарями в цикле
+        # if word.lower() == word.lower()[::-1]:
+        #     palindromes[word] = True
+        # else:
+        #     palindromes[word] = False
+    return palindromes
+
+print(get_dict_palindromes(*words))
--- lesson_26.py
+++ lesson_26.py
@@ -1,1 +1,21 @@
 from lesson_25 import *
+# Правила наименования функций
+# get_item_by_id   get_item_by_slug  get_name_by_id
+# check_user_by_id  check_user_by_slug  check_user_by_name
+# позиционность!
+kwargs_print = {
+    "sep": "\n",
+    "end": "\n\n"
+}
+
+name_dict = {
+    "name": "Vladimir",
+    "last_name": "Monin",
+    "age": 30,
+    "city": "Moscow"
+}
+
+
+print(1, sep="\n", end="\n\n")
+
+print(1, **kwargs_print)
