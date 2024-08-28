5f2e041984a27ea8e1689f2959eee117dfdfc0ef
v <v@mail.ru>
2024-08-28 21:05:36 +0500

    lesson_28: Города на функциях

--- hw/hw_6_7.py
+++ hw/hw_6_7.py
@@ -1,3 +1,16 @@
+"""
+Какие тут могут быть функции?
+1. Чтение данных из JSON
+2. Подготовка set Городов
+3. Проверка есть ли город в сете
+4. Проверка выполнения правил игры
+5. Опционально: работа с сетом плохих букв
+6. Опционально: создания функций "Ход человека и ход компьютера"
+7. Main
+8. Запуск всего
+"""
+
+
 import json
 import time
 from random import randint
@@ -5,6 +18,17 @@ from random import randint
 MIN_TIME_LIMIT = 2
 MAX_TIME_LIMIT = 8
 JSON_FILE = "cities.json"
+MESSAGES_DICT = {
+    "input_prompt": "Введите город: ",
+    "city_in_dataset": "Такой город есть в датасете",
+    "city_not_in_dataset": "Такого города нет в датасете",
+    "human_lose": "Человек проиграл",
+    "computer_lose": "Компьютер проиграл",
+    "computer_step": "Ход компьютера",
+    "computer_choice": "Компьютер выбрал город",
+    "wrong_letter": "Неверная буква. Ваш город должен начинаться на другую букву",
+
+}
 # Сохраняем в JSON файл
 # with open("cities.json", "w", encoding="utf-8") as file:
 #     json.dump(cities_list, file, ensure_ascii=False, indent=4)
@@ -13,48 +37,89 @@ JSON_FILE = "cities.json"
 with open(JSON_FILE, "r", encoding="utf-8") as file:
     cities_list = json.load(file)
 
-cities_set = {city['name'] for city in cities_list}
-# print(cities_set)
 
-computer_city = ''
-while True:
-    # ХОД ЧЕЛОВЕКА
+def read_json(file_name: str) -> list:
+    """
+    Чтение данных из JSON файла
+    """
+    with open(file_name, "r", encoding="utf-8") as file:
+        data = json.load(file)
+    return data
 
-    # Ввод города
-    human_city = input("Введите город: ")
-    
-    # Проверка города. Есть ли он в датасете?
-    if human_city.lower() in {city.lower() for city in cities_set}:
-        print("Такой город есть в датасете")
-    
-    else:
-        print("Человек проиграл! Такого города нет в датасете")
-        break
+def get_cities_data(json_data: list[dict]) -> set:
+    """
+    Подготовка set Городов
+    """
+    return {city["name"] for city in json_data}
 
-    # Проверка а был ли ход компьютера?
-    if computer_city:
-        # Ход был. Надо проверять условия игры
-        if human_city[0].lower() == computer_city[-1].lower():
-            print("Условия выполнены. Ход переходит к компьютеру")
+def is_city_in_set(city: str, cities_set: set) -> bool:
+    """
+    Проверка есть ли город в сете
+    """
+    return city.lower() in {city.lower() for city in cities_set}
 
+def check_city_rules(old_city: str, new_city: str) -> bool:
+    """
+    Проверка выполнения правил игры
+    """
+    return old_city[-1].lower() == new_city[0].lower()
+
+
+def get_random_sleep(
+    min_time: int = MIN_TIME_LIMIT, max_time: int = MAX_TIME_LIMIT
+) -> None:
+    """
+    Получение случайного времени
+    """
+    time.sleep(randint(min_time, max_time))
+
+def main():
+    computer_city = ''
+
+    cities_list = read_json(JSON_FILE)
+    cities_set = get_cities_data(cities_list)
+
+    while True:
+        # ХОД ЧЕЛОВЕКА
+
+        # Ввод города
+        human_city = input(MESSAGES_DICT["input_prompt"])
+        
+        # Проверка города. Есть ли он в датасете?
+        if is_city_in_set(human_city, cities_set):
+            print(MESSAGES_DICT["city_in_dataset"])
+        
         else:
-            print(f"Условия не выполнены. Город должен был начинатся с {computer_city[-1].upper()}")
+            print(MESSAGES_DICT["city_not_in_dataset"], MESSAGES_DICT["human_lose"])
+        
             break
 
-    # ХОД КОМПЬЮТЕРА
+        # Проверка а был ли ход компьютера?
+        if computer_city:
+            # Ход был. Надо проверять условия игры
+            if check_city_rules(computer_city, human_city):
+                    print(MESSAGES_DICT["computer_step"])
 
-    # 1. Перебор сета с городами, с поиском подходящего под правила игры города
-    random_time_sleep = randint(MIN_TIME_LIMIT, MAX_TIME_LIMIT)
-    print(f"Я буду думать {random_time_sleep} секунд...")
-    time.sleep(random_time_sleep)
-    for city in cities_set:
-        if human_city[-1].lower() == city[0].lower():
-            computer_city = city
-            print(f"Компьютер выбрал город {computer_city}")
+            else:
+                    print(f"{MESSAGES_DICT['wrong_letter']} {computer_city[-1].upper()}")
+                    break
+
+        # ХОД КОМПЬЮТЕРА
+
+        # 1. Перебор сета с городами, с поиском подходящего под правила игры города
+        get_random_sleep()
+        
+
+        for city in cities_set:
+            if check_city_rules(human_city, city):
+                computer_city = city
+                print(f"{MESSAGES_DICT['computer_choice']} {computer_city}")
+                break
+        else:
+            # Eсли в цикле не отработал break мы попадем сюда
+            print(MESSAGES_DICT["computer_lose"])
             break
-    else:
-        # Eсли в цикле не отработал break мы попадем сюда
-        print("Компьютер проиграл! Нет подходящего города")
-        break
 
-    
+        
+if __name__ == "__main__":
+    main()
