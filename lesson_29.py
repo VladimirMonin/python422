4e63725fe7f267a79095e32c2557ccb6d73018f0
v <v@mail.ru>
2024-09-02 21:44:53 +0500

    lesson_29: решение практики

--- lesson_29.py
+++ lesson_29.py
@@ -49,3 +49,58 @@ upper_product_list2: Iterable = map(get_upper, product_list)
 Применяет функцию к каждому элементу итерируемого объекта.
 """
 
+data = ((1, 2), (3, 4), (5, 6))
+
+# Цикл с sum
+result = []
+
+for item in data:
+    result.append(sum(item))
+
+# Получить суммы
+result = list(map(sum, data))
+
+
+# Вариант с циклом
+result = []
+
+for item in data:
+    num1 = item[0]
+    num2 = item[1]
+    result.append(num1 + num2)
+
+# Как это сделать через анонимную функцию с 2 аргументами?
+result2 = list(map(lambda item: item[0] + item[1], data))
+
+
+user_input = input("Введите числа через пробел: ").split()
+
+user_input_nums = list(map(int, user_input))
+print(user_input_nums)
+user_input_nums = list(map(lambda x: int(x), user_input))
+print(user_input_nums)
+user_input_nums = [int(num) for num in user_input]
+print(user_input_nums)
+
+user_input_nums = []
+for num in user_input:
+    user_input_nums.append(int(num))
+
+print(user_input_nums)
+
+"""
+Iterable - итерируемый объект.
+- str
+- list
+- tuple
+- dict
+- set
+- range
+- mapobject
+- filterobj
+- zipobject
+- enumerate
+- keysobject
+- valuesobject
+- itemsobject
+"""
