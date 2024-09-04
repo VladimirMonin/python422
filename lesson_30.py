b2e8817c0c6d4f37848f8638eddc5fabfc0d7571
v <v@mail.ru>
2024-09-04 21:41:36 +0500

    lesson_30: фильтрация словаря

--- lesson_30.py
+++ lesson_30.py
@@ -29,47 +29,107 @@ small_dict: Dict[str, Union[None, int]] = { # Dict[str, None|int]
 """
 from dataset.marvel import small_dict
 
-years_values = set(small_dict.values())
+# years_values = set(small_dict.values())
 
-user_str_year = input("Введите год: ")
-user_year = None
+# user_str_year = input("Введите год: ")
+# user_year = None
 
-if user_str_year.isdigit():
-    user_year = int(user_str_year)
-    if user_year in years_values:
-        print(f"Вы ввели год {user_year}. Он валиден.")
-    else:
-        print(f"Вы ввели год {user_year}. Он не валиден.")
-        input('Введите Enter для выхода')
-        exit()
+# if user_str_year.isdigit():
+#     user_year = int(user_str_year)
+#     if user_year in years_values:
+#         print(f"Вы ввели год {user_year}. Он валиден.")
+#     else:
+#         print(f"Вы ввели год {user_year}. Он не валиден.")
+#         input('Введите Enter для выхода')
+#         exit()
 
-else:
-    print(f"Вы ввели чушь.")
-    input('Введите Enter для выхода')
-    exit()
-
-
-if user_year:
-    # Делеаем фильтрацию через цикл
-    result = []
-    result_dict = {}
-    # items - вернет Iterable [(key, value), (key, value), (key, value)]
-    for film, year in small_dict.items():
-        if year == user_year:
-            result.append(film)
-            result_dict[film] = year
+# else:
+#     print(f"Вы ввели чушь.")
+#     input('Введите Enter для выхода')
+#     exit()
 
 
-print(result)
+# if user_year:
+#     # Делеаем фильтрацию через цикл
+#     result = []
+#     result_dict = {}
+#     # items - вернет Iterable [(key, value), (key, value), (key, value)]
+#     for film, year in small_dict.items():
+#         if year == user_year:
+#             result.append(film)
+#             result_dict[film] = year
+
+
+# print(result)
+# print(result_dict)
+
+# # Делаем фильтрацию через фильтр
+
+# # Список фильмов
+# result_list = list(filter(lambda item: item[1] == user_year, small_dict.items()))
+# result_list_final = [film[0] for film in result_list]
+# print(result_list)
+# print(result_list_final)
+
+# result_dict = dict(filter(lambda item: item[1] == user_year, small_dict.items()))
+# print(result_dict)
+
+
+"""
+1. Сделайте константу 
+YEAR_LIMIT = 2020
+2. Попробуйте адаптировать этот код так, чтобы получить фильмы вышедшие ПОСЛЕ этого
+года
+3. Выведите результат в консоль
+"""
+from typing import Tuple, Union
+
+YEAR_LIMIT = 2020
+def year_filter(item: Tuple[str, Union[None, int]], year_limit=YEAR_LIMIT) -> bool:
+    year = item[1]
+    if isinstance(year, int):
+        return year > year_limit
+    return False
+    
+
+YEAR_LIMIT = 2020
+
+result_dict = dict(filter(year_filter, small_dict.items()))
+
+# Вариант с lambda и filter - словарь на выходе
+result_dict = dict(
+    filter(
+        lambda item: item[1] > YEAR_LIMIT if isinstance(item[1], int) else False,
+        small_dict.items(),
+    )
+)
 print(result_dict)
 
-# Делаем фильтрацию через фильтр
 
-# Список фильмов
-result_list = list(filter(lambda item: item[1] == user_year, small_dict.items()))
-result_list_final = [film[0] for film in result_list]
+# Версия Константина (фильтр в фильтре)
+result_dict = dict(
+    filter(
+        lambda item: item[1] > YEAR_LIMIT,
+        filter(
+            lambda x: x[1] != None, 
+            small_dict.items()),
+    )
+)
+
+# Вариант со списковым включением и списков назаний
+
+result_list = [
+    film
+    for film, year in small_dict.items()
+    if isinstance(year, int) and year > YEAR_LIMIT
+]
+
 print(result_list)
-print(result_list_final)
 
-result_dict = dict(filter(lambda item: item[1] == user_year, small_dict.items()))
-print(result_dict)
+# Вариант с словарным включением и словарем
+
+result_dict2 = {
+    film: year
+    for film, year in small_dict.items()
+    if isinstance(year, int) and year > YEAR_LIMIT
+}
