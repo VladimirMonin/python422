08bb0aa4a62fe13aa0d7875509ba5e88094e6a36
v <v@mail.ru>
2024-09-25 22:10:07 +0500

    lesson_34: декораторы возвращающие значение

--- lesson_34.py
+++ lesson_34.py
@@ -62,4 +62,46 @@ def some_func7(name: str, age: int) ->None:
     print(f'Привет {name}, тебе {age} лет')
 
 
-some_func7('Cергей Сергеич', 30) # TypeError: print_decorator2.<locals>.wrapper() takes 1 positional argument but 2 were given
+# some_func7('Cергей Сергеич', 30) # TypeError: print_decorator2.<locals>.wrapper() takes 1 positional argument but 2 were given
+
+
+def print_decorator3(func: Callable) -> Callable:
+    def wrapper(*args, **kwargs):
+        # Что-то делаем до вызова функции
+        print("Перед вызовом функции")
+        func(*args, **kwargs)
+        print("После вызова функции")
+
+    return wrapper
+
+
+@print_decorator3
+def some_func8(name: str, age: int, **kwargs) ->None:
+    print(f'Привет {name}, тебе {age} лет')
+    for key, value in kwargs.items():
+        print(f'{key}: {value}')
+
+
+some_func8('Cергей Сергеич', 30, city='Москва', country='Россия', hoobies=['футбол', 'киберспорт'])
+
+
+# ВОЗВРАТ ЗНАЧЕНИЯ
+
+def print_decorator4(func: Callable) -> Callable:
+    def wrapper(*args, **kwargs):
+        # Что-то делаем до вызова функции
+        print(f'Вызов функции {func.__name__}')
+        result =  func(*args, **kwargs)
+        print(f'Принт после вызова функции {func.__name__}')
+        return result
+
+    return wrapper
+
+
+@print_decorator4
+def some_func9(name: str, age: int) -> str:
+    return f'Привет {name}, тебе {age} лет'
+
+result = some_func9('Cергей Сергеич', 30)
+print(result)
+
