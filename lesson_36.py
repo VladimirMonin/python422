b0f87145053f4d805d9fb66eea5b4f9add5e8f1d
v <v@mail.ru>
2024-10-02 21:55:27 +0500

    lesson_36: геттер и сеттер без декоратора

--- lesson_36.py
+++ lesson_36.py
@@ -17,21 +17,30 @@ _ - protected (защищенные) доступны внутри класса,
 """
 
 class Person():
-    def __init__(self, name: str, age: int):
-        self.__name = name
-        self._age = age
+    def __init__(self, first_name: str, last_name: str, age: int, passport_number: str):
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__age = age
+        self.__passport_number = passport_number
 
-    def __get_info_sring(self):
-        return f'{self.__name} - {self._age}'
+    def __str__(self) -> str:
+        return f"{self.__first_name} {self.__last_name}"
     
-    def show_info(self):
-        info = self.__get_info_sring()
-        print(info)
+    def get_passport_data(self):
+        # Логирование, или другая логика
+        return f"{self.__passport_number}"
+    
+    def set_passport_data(self, passport_number: str):
+        is_all_digit= all(char.isdigit() for char in passport_number.replace(' ', ''))
+        if is_all_digit:
+            self.__passport_number = passport_number
+        else:
+            raise ValueError("Неверный формат паспорта")
+    
+    
+p = Person('Bob', 'Bobov', 20, '123456789')
+print(p)
+print(p.get_passport_data())
+p.set_passport_data('9876543231')
 
-
-p = Person('Bob', 20)
-p._Person__name
-# p.__get_info_sring() # AttributeError: 'Person' object has no attribute '_Person__get_info_sring'
-# result = p._Person__get_info_sring()
-# print(result)
-p.show_info()
+# p.passport_number = 'sdfsdf'
