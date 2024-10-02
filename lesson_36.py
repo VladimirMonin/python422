80d67b37fe54c5352b312d3c963e9f3c234076d7
v <v@mail.ru>
2024-10-02 22:44:21 +0500

    lesson_36: концепция полиморфизма

--- lesson_36.py
+++ lesson_36.py
@@ -4,46 +4,33 @@ Lesson 36: Инкапсуляция
 - Приватные, защищенные и публичные атрибуты класса
 - Приватные, заащищенные и публичные методы класса
 - Геттеры и сеттеры
+- All Any
+- Полиморфизм
 
 
 Практика: pytybefix - библиотека для выгрузки видео с ютуба
 """
 
-"""
-У нас есть возможность спрятать атрибуты или методы класса, чтобы закрыть доступ к ним извне.
-Обычные методы и классы = public (открытые)
-__ - private (закрытые полностью) доступны только внутри класса
-_ - protected (защищенные) доступны внутри класса, а также в наследниках.
-"""
 
-# All - возвращает true если все элементы списка True
-# Any - возвращает true если хотя бы один элемент списка True
+class Cat():
+    def get_voise(self):
+        return 'meow'
+    
 
-integers = [0, 0, 0, 1]
-trus_list = [True, True, False]
+class Dog():
+    def get_voise(self):
+        return 'woof'
+    
 
-is_all_true = all(trus_list)
-is_any_true = any(trus_list)
+class Hamster():
+    def get_voise(self):
+        return f"squeak"
+    
 
+cats = [Cat() for i in range(10)]
+dogs = [Dog() for i in range(5)]
+hamsters = [Hamster() for i in range(20)]
 
-is_all_zeroes = all(x == 0 for x in integers)
-is_any_zeroes = any(x == 0 for x in integers)
+all_animals = cats + dogs + hamsters
 
-# Проверка, все ли строки в списке содержат определенную подстроку
-strings = ["hello world", "hello python", "hello programming"]
-contains_hello = all("hello" in s.lower() for s in strings)
-
-# Проверка, есть ли в списке словарей элемент с определенным значением ключа
-users = [
-    {"name": "Alice", "age": 25},
-    {"name": "Bob", "age": 30},
-    {"name": "Charlie", "age": 35}
-]
-has_adult = any(user["age"] >= 18 for user in users)
-
-# Проверка, все ли числа в списке являются простыми
-def is_prime(n):
-    return n > 1 and all(n % i != 0 for i in range(2, int(n**0.5) + 1))
-
-numbers = [2, 3, 5, 7, 11, 13]
-all_prime = all(is_prime(num) for num in numbers)
+[print(animal.get_voise()) for animal in all_animals]
