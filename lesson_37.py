08dabc1dfe6518a895eb4e91293b8d547c1e2c9f
v <v@mail.ru>
2024-10-07 22:51:07 +0500

    lesson_37: десериализация данных о котах из списка словарей. Множественные атрибуты и валидатор

--- lesson_37.py
+++ lesson_37.py
@@ -2,29 +2,54 @@
 Lesson 37: Наследование
 Все классы в Python наследуются от базового класса object.
 """
+
 class Animal:
-    def __init__(self, name):
+    animal_attrs = ['name', 'color', 'age', 'weight', 'favorite_toy']
+    def __init__(self, name, **kwargs):
         self.name = name
+        for key, value in kwargs.items():
+            self.validate_attr(key)
+            setattr(self, key, value)
 
-    def __str__(self):
-        return f'Это {self.__class__.__name__} с именем {self.name}'
+    def validate_attr(self, attr_name):
+        if attr_name not in self.animal_attrs:
+            raise ValueError(f'Атрибут {attr_name} не поддерживается классом {self.__class__.__name__}')
+
     
-    def voise(self):
-        print(f'{self.name} издает звук')
-
 
 class Cat(Animal):
-    def __init__ (self, name, color):
+    def __init__(self, name, color, **kwargs):
+        super().__init__(name, **kwargs)
         self.color = color
-        super().__init__(name)
-    def voise(self):
-        # Вызовем метод родителя через super()
-        super().voise()
-        # Это можно сделать и напрямую
-        # Animal.voise(self)
-        print(f'{self.name} мяукает')
 
-animal = Animal("Животное")
-cat = Cat("Беляш", "Рыжий")
-print(cat)
-cat.voise()
+# Пример использования
+# cat = Cat("Беляш", "Рыжий", age=3, weight=4.5, favoriteToy="мячик")
+# print(cat)
+
+cats_list = [
+    {
+        "name": "Барсик",
+        "color": "Рыжий",
+        "age": 3,
+        "weight": 4.5,
+        "favorite_toy": "мячик"
+    },
+    {
+        "name": "Мурзик",
+        "color": "Серый",
+        "age": 2,
+        "weight": 3.5,
+        "favorite_toy": "клубок"
+    }, {
+        "name": "Мурка",
+        "color": "Черный",
+        "age": 1,
+        "favorite_toy": "Мурзик"
+    }
+]
+
+cats = [Cat(**cat_data) for cat_data in cats_list]
+for cat in cats:
+    print(cat)
+
+    
