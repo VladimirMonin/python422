f7256fd4f910077fdafbc7feb38f3678c5ec6aca
v <v@mail.ru>
2024-10-09 21:22:47 +0500

    lesson_38: создание комплекта матрешек через экземпляр в инициализаторе (композиция)

--- lesson_38.py
+++ lesson_38.py
@@ -14,14 +14,38 @@ class BigMatryoshka:
 
 class MediumMatryoshka(BigMatryoshka):
     def __init__(self):
+        self.big_matryoshka = BigMatryoshka()
         print("Создана средняя матрёшка")
-        super().__init__()
 
 
 class SmallMatryoshka(MediumMatryoshka):
     def __init__(self):
+        self.medium_matryoshka = MediumMatryoshka()
         print("Создана маленькая матрёшка")
-        super().__init__()
 
 
+# Создали маленькую - создали среднюю - создали большую автоматически
 small_matryoshka = SmallMatryoshka()
+small_matryoshka2 = SmallMatryoshka()
+
+# Распечатаем маленькие матрёшки
+print(small_matryoshka)
+print(small_matryoshka2)
+
+# Распечатаем средние матрёшки
+print(small_matryoshka.medium_matryoshka)
+print(small_matryoshka2.medium_matryoshka)
+
+# Распечатаем большие матрёшки
+print(small_matryoshka.medium_matryoshka.big_matryoshka)
+print(small_matryoshka2.medium_matryoshka.big_matryoshka)
+
+"""
+ЗАДАЧА
+Добавить уникальные ID для каждой категории матрешек (атрибут экземпляра)
+Добавить счетчик созданных матрешек (атрибут класса)
+
+На инициализации каждой матрешки:
+1. Мы увеличиваем счетчик созданных матрешек (в атрибуте класса)
+2. На основе него, устанавливаем уникальный ID для каждой матрешки (атрибут экземпляра)
+"""
