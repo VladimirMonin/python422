ccd56c1bd990e2d0f3ad9a8522ce24c90a1ad25f
v <v@mail.ru>
2024-10-14 21:10:06 +0500

    lesson_39: разные варианты вызова методов родителей

--- lesson_39.py
+++ lesson_39.py
@@ -7,16 +7,31 @@ class A:
 class B:
     def greet(self):
         print('Привет из класса B')
+        # Это обозначается как ошибка, но когда B попадает в C там
+        # Есть вышестоящий метод greet() из класса A
+        # super().greet()
 
 
 class C(B, A):
     
     def greet(self):
         print('Привет из класса C')
+        # Вызываем метод greet() из класса B
+        # super().greet()
+        # A.greet(self)
+        # B.greet(self)
+        
+        # Еще один способ вызвать метод вышестоящего класса
+        super(C, self).greet()
+        super(B, self).greet()
+        
+
     pass
 
 c = C()
 c.greet()
 
 # MRO - Method Resolution Order - Метод разрешения порядка
+# self.__class__.__mro__[1].greet(self) - как один из способов вызвать метод из класса B
 print(C.mro())
+print(C.__mro__[2].__name__)
