3900489da71a4fafce23431dc66ef3b3b91f8597
v <v@mail.ru>
2024-10-14 21:49:58 +0500

    lesson_39: работа с инициализаторами родителей в множественном наследовании

--- lesson_39.py
+++ lesson_39.py
@@ -1,13 +1,17 @@
 # Lesson 39: Множественное наследование. MRO. Миксины
-
+import time
 class A:
 
     def __init__(self, name, age) -> None:
         self.name = name
         self.age = age
+        self.time = time.time()
         
     def greet(self):
         print(f'Привет из A. Name: {self.name}, age: {self.age}')
+
+    def bye(self):
+        print(f'Пока из A. Name: {self.name}, age: {self.age}, time: {self.time}')
 
 class B:
     def __init__(self, name, age) -> None:
@@ -16,31 +20,20 @@ class B:
         
     def greet(self):
         print(f'Привет из B. Name: {self.name}, age: {self.age}')
-        # Это обозначается как ошибка, но когда B попадает в C там
-        # Есть вышестоящий метод greet() из класса A
-        # super().greet()
+
 
 
 class C(B, A):
+    def __init__(self, name, age) -> None:
+        # "Оттолкнись от B в MRO и вызови A __init__"
+        super(B, self).__init__(name, age)
     
     def greet(self):
         print(f'Привет из C. Name: {self.name}, age: {self.age}')
-        # Вызываем метод greet() из класса B
-        # super().greet()
-        # A.greet(self)
-        # B.greet(self)
+        super(C, self).greet() # super().greet() Вызывается метод greet класса B
+        super(B, self).greet() # Вызывается метод greet класса А
         
-        # Еще один способ вызвать метод вышестоящего класса
-        super(C, self).greet()
-        super(B, self).greet()
-        
-
-    pass
 
 c = C('Alex', 25)
 c.greet()
-
-# MRO - Method Resolution Order - Метод разрешения порядка
-# self.__class__.__mro__[1].greet(self) - как один из способов вызвать метод из класса B
-print(C.mro())
-print(C.__mro__[2].__name__)
+c.bye()
