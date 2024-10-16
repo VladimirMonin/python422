b19fb97f482797b60af5cdde0d995edd5282e8e8
v <v@mail.ru>
2024-10-16 23:29:01 +0500

    lesson_40: классы пиццы

--- lesson_40.py
+++ lesson_40.py
@@ -4,49 +4,67 @@ Lesson 40
 1. __call__
 2. __bool__
 3. __len__
+4. __repr__
+
+5. Математика и сравнение
+6. Dataclass
+Сериализация и десериализация через eval()
 """
 
-class Person:
-    def __init__(self, name: str, height:int, is_working: bool):
+# Пицца, ПиццаСоздатель __call__
+
+
+class Pizza:
+    
+    names = ['Маргарита', 'Пепперони', 'Гавайская']
+    def __init__(self, name: str, price: float, size: int):
         self.name = name
-        self.is_working = is_working
-        self.height = height
+        self.price = price
+        self.size = size
+        self.done = False
 
-    def __bool__(self):
-        return self.is_working
-    
-    def __len__(self):
-        return self.height
-    
-    def __str__(self) -> str:
-        return f'{self.name} {self.height} {self.is_working}'
-    
-    def __repr__(self) -> str:
-        return self.__str__()
+    def __len__(self) -> int:
+        return self.size
+
+    def __bool__(self) -> bool:
+        return self.done
     
 
-p = Person('Евгений', 175, True)
+# Создатель пиццы. Call - создает пиццу принимая название, цену и размер
 
-if p:
-    print(f'{p.name} работает')
-else:
-    print(f'{p.name} не работает')
+class PizzaCreator:
+    def __call__(self) -> Pizza|None:
+        self.print_menu()
+        name = input('Введите название пиццы: ')
+        price = float(input('Введите цену пиццы: '))
+        size = int(input('Введите размер пиццы: '))
 
-persons_list = [
-    Person('Евгений', 175, True),
-    Person('Ольга', 166, False),
-    Person('Владимир', 182, True),
-    Person('Наталия', 171, False),
-]
+        if self.validate_name(name):
+            return Pizza(name, price, size)
 
-# Фильтр
-persons_list_filter = list(filter(bool, persons_list))
-# persons_list2 = list(filter(lambda p: p.is_working, persons_list))
+    
+    def print_menu(self):
+        for pizza in Pizza.names:
+            print(pizza)
 
-# Сортировка по росту
-persons_list.sort(key=len)
+    def validate_name(self,  name: str) -> bool:
+        if not name:
+            raise ValueError('Название пиццы не может быть пустым')
+        if not name.isalpha():
+            raise ValueError('Название пиццы должно содержать только буквы')
+        if name not in Pizza.names:
+            raise ValueError('Такой пиццы нет в меню')
+        
+        return True
 
-print(persons_list_filter)
-print(persons_list)
+
+pc = PizzaCreator()
+pizza = pc()
 
 
+p1 = Pizza('Маргарита', 100, 30)
+p2 = Pizza('Пепперони', 150, 30)
+p3 = Pizza('Гавайская', 120, 30)
+
+"Pizza('Маргарита', 100, 30), Pizza('Пепперони', 150, 30), Pizza('Гавайская', 120, 30)"
+repr
