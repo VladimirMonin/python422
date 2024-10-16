be10c52756f0465b15304eedf54e86590381635b
v <v@mail.ru>
2024-10-16 23:08:50 +0500

    lesson_40: магический метод __len__ и сортировка людей по возрасту

--- lesson_40.py
+++ lesson_40.py
@@ -6,17 +6,47 @@ Lesson 40
 3. __len__
 """
 
-class File:
-    def __init__(self, path):
-        self.path = path
+class Person:
+    def __init__(self, name: str, height:int, is_working: bool):
+        self.name = name
+        self.is_working = is_working
+        self.height = height
 
     def __bool__(self):
-        return bool(self.path)
+        return self.is_working
+    
+    def __len__(self):
+        return self.height
+    
+    def __str__(self) -> str:
+        return f'{self.name} {self.height} {self.is_working}'
+    
+    def __repr__(self) -> str:
+        return self.__str__()
+    
 
+p = Person('Евгений', 175, True)
 
-file = File('')
-
-if file
-    print('Всегда да')
+if p:
+    print(f'{p.name} работает')
 else:
-    print('нет')
+    print(f'{p.name} не работает')
+
+persons_list = [
+    Person('Евгений', 175, True),
+    Person('Ольга', 166, False),
+    Person('Владимир', 182, True),
+    Person('Наталия', 171, False),
+]
+
+# Фильтр
+persons_list_filter = list(filter(bool, persons_list))
+# persons_list2 = list(filter(lambda p: p.is_working, persons_list))
+
+# Сортировка по росту
+persons_list.sort(key=len)
+
+print(persons_list_filter)
+print(persons_list)
+
+
