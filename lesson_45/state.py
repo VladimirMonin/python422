4f953e78e4531459f46e1f8a44d6ce0bad1c1b64
v <v@mail.ru>
2024-11-06 22:56:07 +0500

    lesson_45: продолжаем рассматривать состояние

--- lesson_45/state.py
+++ lesson_45/state.py
@@ -2,67 +2,75 @@ from abc import ABC, abstractmethod
 
 class PlayerState(ABC):
     @abstractmethod
-    def play(self, player):
+    def play(self, player: "MusicPlayer": "MusicPlayer"):
         pass
 
     @abstractmethod
-    def pause(self, player):
+    def pause(self, player: "MusicPlayer": "MusicPlayer"):
         pass
 
     @abstractmethod
-    def stop(self, player):
+    def stop(self, player: "MusicPlayer": "MusicPlayer"):
         pass
 
 
 class PlayingState(PlayerState):
-    def play(self, player):
+    def play(self, player: "MusicPlayer"):
         print("Уже играет")
     
-    def pause(self, player):
+    # Переход в состояние паузы
+    def pause(self, player: "MusicPlayer"):
         print("Ставим на паузу")
         player.set_state(PausedState())
     
-    def stop(self, player):
+    # Переход в состояние остановки
+    def stop(self, player: "MusicPlayer"):
         print("Останавливаем воспроизведение")
         player.set_state(StoppedState())
 
 
 class PausedState(PlayerState):
-    def play(self, player):
+    # Возобновление проигрывания из состояния паузы
+    def play(self, player: "MusicPlayer"):
         print("Возобновляем воспроизведение")
         player.set_state(PlayingState())
     
-    def pause(self, player):
+    def pause(self, player: "MusicPlayer"):
         print("Уже на паузе")
     
-    def stop(self, player):
+    # Переход в состояние остановки из паузы
+    def stop(self, player: "MusicPlayer"):
         print("Останавливаем воспроизведение")
         player.set_state(StoppedState())
 
 
 class StoppedState(PlayerState):
-    def play(self, player):
+    # Начало воспроизведения из остановленного состояния
+    def play(self, player: "MusicPlayer"):
         print("Начинаем воспроизведение")
         player.set_state(PlayingState())
     
-    def pause(self, player):
+    def pause(self, player: "MusicPlayer"):
         print("Невозможно поставить на паузу остановленный плеер")
     
-    def stop(self, player):
+    def stop(self, player: "MusicPlayer"):
         print("Уже остановлено")
 
 
 class MusicPlayer:
+    # Инициализация плеера в состоянии остановки
     def __init__(self):
         self._state = StoppedState()
         self.current_track = None
     
+    # Метод для изменения текущего состояния плеера
     def set_state(self, state: PlayerState):
         self._state = state
     
     def get_state(self):
         return self._state
     
+    # Делегирование выполнения команды текущему состоянию
     def play(self):
         self._state.play(self)
     
@@ -74,23 +82,34 @@ class MusicPlayer:
 
 
 def test_player():
+    # Создаем экземпляр плеера в начальном состоянии STOP
     player = MusicPlayer()
     
     # Тестируем из состояния STOP
     print("Состояние STOP:")
+    # Запускаем воспроизведение из состояния STOP
     player.play()  # -> PLAYING
+    # Останавливаем воспроизведение, возвращаемся в STOP
     player.stop()  # -> STOP
+    # Проверяем невозможность постановки на паузу в состоянии STOP
     player.pause() # Ошибка
     
     print("\nСостояние PLAYING:")
+    # Переходим в состояние воспроизведения
     player.play()  # -> PLAYING
+    # Проверяем повторный вызов play в состоянии воспроизведения
     player.play()  # Уже играет
+    # Ставим на паузу из состояния воспроизведения
     player.pause() # -> PAUSE
     
     print("\nСостояние PAUSE:")
+    # Проверяем повторную постановку на паузу
     player.pause() # Уже на паузе
+    # Возобновляем воспроизведение из состояния паузы
     player.play()  # -> PLAYING
+    # Останавливаем воспроизведение полностью
     player.stop()  # -> STOP
 
 if __name__ == "__main__":
+
     test_player()
